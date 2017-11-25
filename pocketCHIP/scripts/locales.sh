abf0b0d05f2693248fd6d510eeb7a2878ae4caff
Stanislaw Alam <stan.alam@gmail.com>
2017-11-25 15:40:16 +0000

    typo on line3 fixed for localesdef operation

--- pocketCHIP/scripts/locales.sh
+++ pocketCHIP/scripts/locales.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#apt-get install -y locales locales-all
-localdef -i en_US -f UTF-8 en_US.UTF-8
+apt-get install -y locales locales-all
+localedef -i en_US -f UTF-8 en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
