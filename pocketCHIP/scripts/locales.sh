6aea81c8a733cb4bdcce663fd3d00588270b3429
Stanislaw Alam <stan.alam@gmail.com>
2017-11-25 15:28:47 +0000

    Just added a script to create proper local config for erlang and elixir

---
+++ pocketCHIP/scripts/locales.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#apt-get install -y locales locales-all
+localdef -i en_US -f UTF-8 en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
